baf1d0abb3c5804e30dea7d5358b17eb87eb27a9
ahmadauf98 <ahmadauf98@gmail.com>
2020-08-15 03:37:16 +0800

    Initial Commit

---
+++ layouts/default.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div id="Navbar">
+      <Navbar />
+    </div>
+    <div id="Body">
+      <Nuxt />
+    </div>
+  </div>
+</template>
+
+<script>
+import Navbar from '~/components/layouts/navbar.vue'
+export default {
+  components: {
+    Navbar,
+  },
+}
+</script>
+
+<style>
+#Navbar {
+  background-color: #fffffe;
+  padding: 1em 0em;
+}
+
+#Body{
+  overflow-x: hidden;
+}
+</style>
