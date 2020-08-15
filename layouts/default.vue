e848461cb160bdd128fc4eb60060c0ccc14d2870
ahmadauf98 <ahmadauf98@gmail.com>
2020-08-16 00:28:37 +0800

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,14 +6,19 @@
     <div id="Body">
       <Nuxt />
     </div>
+    <div id="Footer">
+      <Footer />
+    </div>
   </div>
 </template>
 
 <script>
 import Navbar from '~/components/layouts/navbar.vue'
+import Footer from '~/components/layouts/footer.vue'
 export default {
   components: {
     Navbar,
+    Footer,
   },
 }
 </script>
@@ -24,7 +29,18 @@ export default {
   padding: 1em 0em;
 }
 
-#Body{
+#Body {
   overflow-x: hidden;
+}
+
+#Footer {
+  background-color: #232127;
+}
+
+.lead {
+  font-weight: 500;
+  font-size: 18px;
+  line-height: 25px;
+  color: #9e9aa7;
 }
 </style>
